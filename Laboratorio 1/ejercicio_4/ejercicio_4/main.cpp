94f19b6f05f6094a8ec38b5a517657662331b4ec
Angel Espinosa David <angel.espinosa@udea.edu.co>
2025-03-29 21:01:09 -0500

    Ejecicio 4 y comando git

---
+++ Laboratorio 1/ejercicio_4/ejercicio_4/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{int n1, n2;
+    cout << "Programa para detectar menor o mayor" << endl;
+    cout << "Ingrese en primer numero";
+    cin >>n1;
+    cout << "Ingrese en segundo numero";
+    cin >>n2;
+    return 0;
+}
