b19a3c41f85b6c8d6e86ed74fabd39cd4818c2b8
Angel Espinosa David <angel.espinosa@udea.edu.co>
2025-03-30 00:28:22 -0500

    Ejecicio hasta el 6

---
+++ Laboratorio 1/ejercicio_5/ejercicio_5/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n1,n2,resul=0;
+    cout << "Ejercicio 5 A/B con redondeo" << endl;
+    cout << "Ingrese el  primer numero: "<<endl;
+    cin>> n1;
+    cout<<" Ingrese el segundo numero: "<<endl;
+    cin>>n2;
+    resul=n1/n2;
+    cout<<"El resultado es "<<resul<<endl;
+
+    return 0;
+}
