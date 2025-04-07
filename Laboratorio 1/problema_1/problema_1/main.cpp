587b03cc78cab4ad99390e4655060f3a806b0007
Angel Espinosa David <angel.espinosa@udea.edu.co>
2025-04-07 00:06:10 -0500

    Problema 9 con errores

--- Laboratorio 1/problema_1/problema_1/main.cpp
+++ Laboratorio 1/problema_1/problema_1/main.cpp
@@ -3,20 +3,25 @@
 using namespace std;
 
 int main()
-{ char a;
+{ char a, i;
+    bool vocal=false;
     char vocales[5]={'a','e','i','o','u'};
     char consonantes[]={'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z'};
 
     cout << "Ingrese un letra" << endl;
     cin>>a;
-    for (int i=0;i<=25;i++){
+    for (int i=0;i<=5;i++){
         if (vocales[i]==a){
-        cout <<"es vocal"<<a<<vocales[i]<<i<<endl;
+            vocal=true;
+            break;
         }
 
-        else{
-            cout <<"no es vocal"<<endl;
-        }
+
 }
+    if(vocal){
+    cout <<"Es vocal"<<endl;}
+    else{
+        cout <<"No es vocal"<<endl;
+    }
     return 0;
 }
