29e97bcbcfc7af5423b1463844697dd43e238402
Angel Espinosa David <angel.espinosa@udea.edu.co>
2025-04-15 23:31:22 -0500

    ejercicio 12, practicando logica

--- Laboratorio 1/ejercicio_12/ejercicio_12/main.cpp
+++ Laboratorio 1/ejercicio_12/ejercicio_12/main.cpp
@@ -4,6 +4,15 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    int N,result,i;
+    result=1;
+
+    cout << "Programa que eleva la potencia hasta 5 veces" << endl;
+    cin>>N;
+    for (i = 1; i <= 5; ++i) {
+        result=result*N;
+        cout<<N<<"*"<<"="<<result<<endl;
+    }
+
     return 0;
 }
