c9faf71f64c99cdbafea488a6ad33376e6e047c7
Angel Espinosa David <angel.espinosa@udea.edu.co>
2025-04-16 22:33:51 -0500

    Practicas para trbajar la logica

---
+++ Laboratorio 1/ejercicio_14/ejercicio_14/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i, j;
+    cout << "Imprime dos columnas, una desde el 1, otra des de 50 hacia atras" << endl;
+
+
+    for (int i = 1, j=50;i <= 50; ++i,--j) {
+
+        cout<<i<<".........."<<j<<endl;
+
+    }
+    return 0;
+}
