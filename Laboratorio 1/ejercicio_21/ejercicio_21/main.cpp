987259bcf47224ccd1cfb338605bf282b4648ffb
Angel Espinosa David <angel.espinosa@udea.edu.co>
2025-04-18 16:06:10 -0500

    Practicas para trabajar la logica

--- Laboratorio 1/ejercicio_21/ejercicio_21/main.cpp
+++ Laboratorio 1/ejercicio_21/ejercicio_21/main.cpp
@@ -4,6 +4,19 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    char letra;
+
+    cout << "Convertir a mayuscula o viceversa" << endl;
+    cout<<"ingresar una letra"<<endl;
+    cin>>letra;
+    if(letra>='a'&&letra<='z'){
+        letra=letra - 32;
+        cout<<"Letra convertida: "<<letra<<endl;
+
+    }
+    else if(letra>='A'&&letra<='Z') {
+            letra=letra + 32;
+            cout<<"Letra convertida: "<<letra<<endl;
+        }
     return 0;
 }
