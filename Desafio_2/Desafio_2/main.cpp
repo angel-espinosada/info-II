34ea23a0716d2d474fee06436d6fdf012aaad7ee
Angel Espinosa David <angel.espinosa@udea.edu.co>
2025-05-21 00:07:25 -0500

    EMpezando el trabajo

---
+++ Desafio_2/Desafio_2/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include<string>
+#include<alojamiento.h>
+
+
+using namespace std;
+
+class nombre_alojamiento{
+
+
+
+
+public:
+    void alojamiento();
+};
+
+int main()
+{
+    cout << "Hello World!" << endl;
+    return 0;
+}
