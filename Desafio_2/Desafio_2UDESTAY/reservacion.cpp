809b0a577acd59b7920dac1f4a9d797a82e5c945
ángel-espinosada <angel.espinosa@udea.edu.co>
2025-05-28 12:39:20 -0500

    Se logra sin formatear el texto

--- Desafio_2/Desafio_2UDESTAY/reservacion.cpp
+++ Desafio_2/Desafio_2UDESTAY/reservacion.cpp
@@ -1,8 +1,14 @@
 #include "reservacion.h"
 #include <iostream>
-
+#include <sstream>
 using namespace std;
-
+string quitarEspacios(const string& s) {
+    string limpio = "";
+    for (int i = 0; i < s.length(); ++i) {
+        if (s[i] != ' ') limpio += s[i];
+    }
+    return limpio;
+}
 // Declaración de función externa
 extern bool leerArchivoTexto(const string& nombreArchivo, string* &lineas, int &cantidadLineas);
 
@@ -42,76 +48,100 @@ string Reservacion::toLineaTexto() {
 
 // Mostrar reservaciones de un anfitrión
 void Reservacion::verReservacionesAnfitrion(const string& documentoAnfitrion) {
+    //cout << "\n[DEBUG] Entrando a verReservacionesAnfitrion con doc: " << documentoAnfitrion << endl;
+
     string* lineas = nullptr;
     int cantidad = 0;
 
     // 1. Leer anfitriones.txt
     if (!leerArchivoTexto("anfitriones.txt", lineas, cantidad)) {
-        cout << "No se pudo leer anfitriones.txt\n";
+        cout << "Error al leer anfitriones.txt\n";
         return;
     }
 
-    string* codigosAlojamiento = new string[20];
-    int totalAlojamientos = 0;
-
+    // 2. Buscar la línea del anfitrión
+    string lineaAnfitrion = "";
     for (int i = 0; i < cantidad; ++i) {
         if (lineas[i].substr(0, documentoAnfitrion.length()) == documentoAnfitrion) {
-            int pos = 0, guion = 0, count = 0;
-            while (count < 4 && (guion = lineas[i].find('-', pos)) != -1) {
-                pos = guion + 1;
-                count++;
-            }
-
-            string alojamientosStr = lineas[i].substr(pos);
-            int ini = 0, coma;
-            while ((coma = alojamientosStr.find(',', ini)) != -1) {
-                codigosAlojamiento[totalAlojamientos++] = alojamientosStr.substr(ini, coma - ini);
-                ini = coma + 1;
-            }
-            if (ini < alojamientosStr.length()) {
-                codigosAlojamiento[totalAlojamientos++] = alojamientosStr.substr(ini);
-            }
+            lineaAnfitrion = lineas[i];
             break;
         }
     }
-
     delete[] lineas;
 
-    if (totalAlojamientos == 0) {
-        cout << "No se encontraron alojamientos para el anfitrión.\n";
-        delete[] codigosAlojamiento;
+    if (lineaAnfitrion == "") {
+        cout << "No se encontró al anfitrión.\n";
         return;
     }
 
-    // 2. Leer reservas.txt
+    //cout << "[DEBUG] Línea del anfitrión encontrada: " << lineaAnfitrion << endl;
+
+    // 3. Extraer los códigos de alojamiento
+    int conteoComas = 0;
+    for (int i = 0; i < lineaAnfitrion.length(); ++i)
+        if (lineaAnfitrion[i] == ',') conteoComas++;
+
+    int cantidadAlojamientos = conteoComas + 1;
+    string* alojamientos = new string[cantidadAlojamientos];
+
+    // Saltar 4 campos
+    int pos = 0;
+    for (int i = 0; i < 4; ++i) {
+        while (pos < lineaAnfitrion.length() && lineaAnfitrion[pos] != '-') pos++;
+        pos++;
+    }
+
+    int inicio = pos, idx = 0;
+    for (int i = pos; i <= lineaAnfitrion.length(); ++i) {
+        if (lineaAnfitrion[i] == ',' || lineaAnfitrion[i] == '\0') {
+            alojamientos[idx++] = quitarEspacios(lineaAnfitrion.substr(inicio, i - inicio));
+            inicio = i + 1;
+        }
+    }
+
+    //cout << "[DEBUG] Códigos de alojamiento encontrados:\n";
+    for (int i = 0; i < cantidadAlojamientos; ++i)
+        cout << "   - " << alojamientos[i] << endl;
+
+    // 4. Leer reservas.txt
     lineas = nullptr;
     cantidad = 0;
+
     if (!leerArchivoTexto("reservas.txt", lineas, cantidad)) {
         cout << "No se pudo leer reservas.txt\n";
-        delete[] codigosAlojamiento;
+        delete[] alojamientos;
         return;
     }
 
-    cout << "\n=== Reservaciones encontradas ===\n";
     bool encontro = false;
+    cout << "\n=== Reservaciones encontradas ===\n";
 
     for (int i = 0; i < cantidad; ++i) {
-        int pos = 0;
-        string campos[5];
-        for (int j = 0; j < 5; ++j) {
-            int guion = lineas[i].find('-', pos);
-            if (guion == -1) {
-                campos[j] = lineas[i].substr(pos);  // resto de línea
-                break;
-            }
-            campos[j] = lineas[i].substr(pos, guion - pos);
-            pos = guion + 1;
-        }
+        //cout << "[DEBUG] Analizando línea de reserva: " << lineas[i] << endl;
 
-        string codAloj = campos[3];
-        for (int j = 0; j < totalAlojamientos; ++j) {
-            if (codAloj == codigosAlojamiento[j]) {
-                cout << "→ " << lineas[i] << endl;
+        // Extraer código de alojamiento usando stringstream
+        // Extraer código de alojamiento correctamente
+        stringstream ss(lineas[i]);
+        string codRes, anio, mes, dia, noches, codAloj;
+        getline(ss, codRes, '-');
+        getline(ss, anio, '-');
+        getline(ss, mes, '-');
+        getline(ss, dia, '-');
+        getline(ss, noches, '-');
+        getline(ss, codAloj, '-');
+
+        // Si necesitás la fecha completa:
+        //string fechaInicio = anio + "-" + mes + "-" + dia;
+
+        codAloj = quitarEspacios(codAloj);
+        //cout << "[DEBUG] Código alojamiento de reserva: '" << codAloj << "'\n";
+
+
+        // Comparar con los códigos del anfitrión
+        for (int j = 0; j < cantidadAlojamientos; ++j) {
+            //cout << "[DEBUG] Comparando con: '" << alojamientos[j] << "'\n";
+            if (alojamientos[j] == codAloj) {
+                cout << "Item " << lineas[i] << endl;
                 encontro = true;
                 break;
             }
@@ -122,6 +152,6 @@ void Reservacion::verReservacionesAnfitrion(const string& documentoAnfitrion) {
         cout << "No hay reservaciones activas para sus alojamientos.\n";
     }
 
+    delete[] alojamientos;
     delete[] lineas;
-    delete[] codigosAlojamiento;
 }
